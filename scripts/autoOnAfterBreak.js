46887a158a5404a273e391c08cdf5a2395bafd58
celso yamashita <ctyamashita@hotmail.com>
2025-11-12 15:27:07 +0900

    added icon and fixed onDuty bug

--- scripts/autoOnAfterBreak.js
+++ scripts/autoOnAfterBreak.js
@@ -18,34 +18,38 @@ function autoOnAfterBreak() {
       if (dayPassed) chrome.storage.sync.set({onDuty: false})
       
       // setting observer
-      const observer = new MutationObserver(async(mutationList, _observer) => {
+      const observer = new MutationObserver((mutationList, _observer) => {
         for (const mutation of mutationList) {
           // console.log("This element changed:", mutation.target)
-          const { onDuty } = await chrome.storage.sync.get('onDuty')
           // check if it's a remaining time update
           const isRemainingTimeEl = mutation.target?.parentElement?.parentElement?.classList?.contains('info-bubble')
           // target(span) > strong > div.info-bubble
-          if (mutation.type == 'attributes' && isRemainingTimeEl && autoOnAfterBreak && onDuty) {
-            const isStart = mutation.target.innerText.includes('hour')
-            let remainingTime = mutation.target.innerText.match(/\d+/)
-            // finding remaining time
-            if (remainingTime) {
-              remainingTime = Number(remainingTime[0])
-            } else {
-              remainingTime = isStart ? 60 : 0
-            }
-
-            // check if already online
-            const alreadyOnline = !!document.querySelector('.is-on-duty')
-            if (alreadyOnline) return
-
-            if (remainingTime == 0) {
-              const toggle = document.querySelector('a:has(.switch)')
-              toggle.click();
-              location.reload();
-            } else {
-              // console.log('Remaining time:', remainingTime)
-            }
+          if (mutation.type == 'attributes' && isRemainingTimeEl && autoOnAfterBreak) {
+            chrome.storage.sync.get('onDuty').then(onDutyResp=>{
+              const {onDuty} = onDutyResp
+              if (onDuty) {
+                const isStart = mutation.target.innerText.includes('hour')
+                let remainingTime = mutation.target.innerText.match(/\d+/)
+                // finding remaining time
+                if (remainingTime) {
+                  remainingTime = Number(remainingTime[0])
+                } else {
+                  remainingTime = isStart ? 60 : 0
+                }
+    
+                // check if already online
+                const alreadyOnline = !!document.querySelector('.is-on-duty')
+                if (alreadyOnline) return
+    
+                if (remainingTime == 0) {
+                  const toggle = document.querySelector('a:has(.switch)')
+                  toggle.click();
+                  location.reload();
+                } else {
+                  // console.log('Remaining time:', remainingTime)
+                }
+              }
+            })
           } else if (mutation.type == 'attributes' && mutation.target.classList.contains('switch-container')) {
             // if toggle change
             const toggleValue = mutation.target.classList.contains('is-on-duty')
