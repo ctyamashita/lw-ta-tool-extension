d9ce4208eb14cbed31d76b0686a6d3ab3d6fe656
celso yamashita <ctyamashita@hotmail.com>
2025-12-23 09:36:14 -0300

    fixed css issue

--- scripts/copyCSS.js
+++ scripts/copyCSS.js
@@ -1,6 +1,9 @@
-function copyCSS() {
-  const stylesheets = Array.from(document.head.querySelectorAll('link[rel="stylesheet"]')).map(el=>el.href);
-  chrome.storage.sync.set({css: stylesheets})
+async function copyCSS() {
+  const { css } = await chrome.storage.sync.get("css")
+  if (!css) {
+    const stylesheets = Array.from(document.head.querySelectorAll('link[rel="stylesheet"]')).map(el=>el.href);
+    if (css !== stylesheets) chrome.storage.sync.set({css: stylesheets})
+  }
 }
 
 copyCSS()
