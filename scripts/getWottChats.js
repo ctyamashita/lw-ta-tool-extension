bd77b63b16dc76c9f2de5088b8f5258ade6b6bb1
celso yamashita <ctyamashita@hotmail.com>
2026-03-12 22:45:19 +0900

    get wottChats and Flashcards only if there is students displayed on the page

--- scripts/getWottChats.js
+++ scripts/getWottChats.js
@@ -1,29 +1,50 @@
 async function getWottChats() {
   const studentsData = document.querySelectorAll('.dashboard-students > div.flexbox');
-  const { currentBatch } = await chrome.storage.local.get('currentBatch')
-  chrome.storage.local.get(currentBatch).then(async response=>{
-    studentsData.forEach(row=>{
-      const student = row.querySelector('.student-name')
-      const wott = row.querySelector('.num-column:last-child')
-      const flashcard = row.querySelector('.num-column:nth-last-child(2)')
-      const wottCount = wott ? Number(wott.innerText) : 0
-      const flashcardCompletion = flashcard ? Number(flashcard.innerText.slice(0,-1)) : 0
-      if (student) {
-        const studentName = student.innerText
-        response[currentBatch].students[studentName] ||= {ticketCount: 0, wottCount: 0, flashcard: 0}
-        response[currentBatch].students[studentName].wottCount = wottCount
-        response[currentBatch].students[studentName].flashcard = flashcardCompletion
+  if (studentsData.length > 1) {
+    const { currentBatch } = await chrome.storage.local.get('currentBatch')
+    chrome.storage.local.get(currentBatch).then(async response=>{
+      studentsData.forEach(row=>{
+        const student = row.querySelector('.student-name')
+        const wott = row.querySelector('.num-column:last-child')
+        const flashcard = row.querySelector('.num-column:nth-last-child(2)')
+        const wottCount = wott ? Number(wott.innerText) : 0
+        const flashcardCompletion = flashcard ? Number(flashcard.innerText.slice(0,-1)) : 0
+        if (student) {
+          const studentName = student.innerText
+          response[currentBatch].students[studentName] ||= {ticketCount: 0, wottCount: 0, flashcard: 0}
+          response[currentBatch].students[studentName].wottCount = wottCount
+          response[currentBatch].students[studentName].flashcard = flashcardCompletion
+        }
+      })
+  
+      localStorage.setItem(currentBatch, JSON.stringify(response[currentBatch]))
+      await chrome.storage.local.set(response)
+  
+      chrome.storage.local.get('collecting').then(response=>{
+        const { collecting } = response
+        if (collecting) window.close()
+      })
+    })
+  } else {
+    chrome.storage.local.get(currentBatch).then(async (response) => {
+      const students = response[currentBatch]
+      for (const studentName in students) {
+        const studentData = students[studentName];
+        studentData.wottCount = 0
+        studentData.flashcard = 0
+        studentData.ticketCount ||= 0
       }
+
+      localStorage.setItem(currentBatch, JSON.stringify(response[currentBatch]))
+      await chrome.storage.local.set(response)
+    
+      chrome.storage.local.get('collecting').then(response=>{
+        const { collecting } = response
+        if (collecting) window.close()
+      })
     })
 
-    localStorage.setItem(currentBatch, JSON.stringify(response[currentBatch]))
-    await chrome.storage.local.set(response)
-
-    chrome.storage.local.get('collecting').then(response=>{
-      const { collecting } = response
-      if (collecting) window.close()
-    })
-  })
+  }
 }
 
 getWottChats()
